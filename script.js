9fdccc791e28099533a34bf8ce99b14abd608e83
geoffreynyc <107085735+geoffreynyc@users.noreply.github.com>
2023-02-11 20:12:15 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+let color = "black";
+let click = false;
+
+document.addEventListener("DOMContentLoaded", function () {
+  createBoard(16);
+
+  document.querySelector("body").addEventListener("click", function (e) {
+    if (e.target.tagName != "BUTTON") {
+      click = !click;
+      let draw = document.querySelector("#draw");
+      if (click) {
+        draw.innerHTML = "Play!";
+      } else {
+        draw.innerHTML = "Paused!";
+      }
+    }
+  });
+
+  let btnPopup = document.querySelector("#popup");
+  btnPopup.addEventListener("click", function () {
+    let size = getSize();
+    createBoard(size);
+  });
+});
+
+function createBoard(size) {
+  let board = document.querySelector(".board");
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.addEventListener("mouseover", colorDiv);
+    board.insertAdjacentElement("beforeend", div);
+  }
+}
+2;
+function getSize() {
+  let input = prompt("Enter the size of the board:");
+  let message = document.querySelector("#message");
+  if (input == "") {
+    message.innerHTML = "Please provide a number between 1-100";
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = "Please provide a number between 1-100";
+  } else {
+    message.innerHTML = "Begin drawing!";
+    return input;
+  }
+}
+
+function colorDiv() {
+  if (click) {
+    if (color == "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = "black";
+    }
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function resetBoard() {
+  let divs = document.querySelectorAll("div");
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
+}
